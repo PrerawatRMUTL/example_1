be4b8d87e4f22dbdc52d8918eefef6d4b593a5cc
PrerawatRMUTL <124560395+PrerawatRMUTL@users.noreply.github.com>
2024-05-16 14:39:05 +0700

    chec

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -5,6 +5,9 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return Scaffold(
+     drawer: Drawer(),
+     appBar: AppBar(title: Text("ddddd")),
+    );
   }
 }
